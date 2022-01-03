0f89e86f37cb7975f00741cbe694ded698f1d600
poljar <poljar@users.noreply.github.com>
2022-01-03 10:18:30 +0000

    deploy: 1ed8b615bad4d30299630c5af338e73a571c46ca

--- implementors/serde/ser/trait.Serialize.js
+++ implementors/serde/ser/trait.Serialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["vodozemac"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.132/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"vodozemac/struct.Curve25519PublicKey.html\" title=\"struct vodozemac::Curve25519PublicKey\">Curve25519PublicKey</a>","synthetic":false,"types":["vodozemac::types::Curve25519PublicKey"]}];
+implementors["vodozemac"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.133/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"vodozemac/struct.Curve25519PublicKey.html\" title=\"struct vodozemac::Curve25519PublicKey\">Curve25519PublicKey</a>","synthetic":false,"types":["vodozemac::types::Curve25519PublicKey"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
