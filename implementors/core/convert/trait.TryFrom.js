3675bce4898dbb3da614e0301f47c78d073fee93
poljar <poljar@users.noreply.github.com>
2022-01-07 15:27:43 +0000

    deploy: 9ff215a011ba1408c888ebf0d79d96d2f8722799

---
+++ implementors/core/convert/trait.TryFrom.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["vodozemac"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"enum\" href=\"vodozemac/olm/enum.MessageType.html\" title=\"enum vodozemac::olm::MessageType\">MessageType</a>","synthetic":false,"types":["vodozemac::olm::messages::MessageType"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
