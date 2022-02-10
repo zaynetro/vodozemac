75068fed33da2cdd8dce2b019135dbd861eded14
poljar <poljar@users.noreply.github.com>
2022-02-10 08:33:56 +0000

    deploy: 24ce0fe043a3531906231c2aa3d236ed98f3a991

---
+++ implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["vodozemac"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"vodozemac/struct.KeyId.html\" title=\"struct vodozemac::KeyId\">KeyId</a>","synthetic":false,"types":["vodozemac::types::KeyId"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
