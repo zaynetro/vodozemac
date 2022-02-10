96141197fe92e7e20d024170e47e4e9b05e90f5e
poljar <poljar@users.noreply.github.com>
2022-02-10 08:33:59 +0000

    deploy: 38c5c81e7124a82be5b92d83263b7d6d15cfcbbf

--- implementors/core/cmp/trait.PartialOrd.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["vodozemac"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"vodozemac/struct.KeyId.html\" title=\"struct vodozemac::KeyId\">KeyId</a>&gt; for <a class=\"struct\" href=\"vodozemac/struct.KeyId.html\" title=\"struct vodozemac::KeyId\">KeyId</a>","synthetic":false,"types":["vodozemac::types::KeyId"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
